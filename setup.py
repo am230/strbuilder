cd00bc1aa55226481803b4d805c1b88b4a926373
nardpw <111672334+am230@users.noreply.github.com>
2023-02-22 15:55:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ name = 'strbuilder'
 
 setup(
     name=name,
-    version="1.0.3",
+    version="1.0.4",
     keywords=("builder"),
     description="A simple string builder",
     long_description=long_description,

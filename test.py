2670f2bfec20ecf968abe4b7992b3bd7ac04f90a
nardpw <111672334+am230@users.noreply.github.com>
2023-03-16 09:36:08 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from strbuilder import Builder, SurroundBuilder
+
+condition = False
+
+print(
+    Builder('base', separator='\n')
+    .write('header')
+    .write(Builder(separator=', ')
+        .write('aaa')
+        .write_if(condition, 'bbb')
+        .write_if(condition, 'ccc', or_else='ddd'))
+    .write(SurroundBuilder(surround='{}')
+        .write('surrounded!'))
+    .write('footer')
+    .build()
+)
